a7354cf1b8537e1c4dee4321f91a5dce936621ed
hoschi <vollekannehoschi@googlemail.com>
2010-11-03 15:28:22 +0100

    ... rausfinden wie parser tickt

--- test/block1/A1RelationalQuery.java
+++ test/block1/A1RelationalQuery.java
@@ -1,6 +1,24 @@
 package test.block1;
 
+import java.io.StringReader;
+
+import org.junit.Test;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
+import parser.gene.SimpleSQLParser;
+import parser.syntaxtree.CompilationUnit;
+
 
 public class A1RelationalQuery {
-
+	
+	@Test
+	public void testForSqlSyntaxTree() throws parser.gene.ParseException {
+		String sql = "select name from user where name like 'blubb'";
+		SimpleSQLParser parser = new SimpleSQLParser(new StringReader(sql));
+		parser.setDebugALL(true);
+		@SuppressWarnings("unused")
+		CompilationUnit cu = parser.CompilationUnit();
+		sql = "truianetirane";
+	}
 }

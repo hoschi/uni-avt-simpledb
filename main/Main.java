f5fe41296065baddd2f2dc927859fa691b408477
Stefan Gojan <vollekannehoschi@gmail.com>
2010-11-09 12:44:07 +0100

    [fixed #6016761] added untested print and read method to Main

--- main/Main.java
+++ main/Main.java
@@ -1,27 +1,45 @@
 package main;
 
 import relationenalgebra.ITreeNode;
+import java.io.*;
+import database.*;
 
 public class Main {
 
 //Verzeichnis der Buchversandsdatenbank	
-public static final String KUNDENDB = "";
+public static final String KUNDENDB = "db";
 
-    public static void main(String[] args){
-    	//TODO Tests...
-    }
+	  public static void main(String[] args){
+		  Main.readFile(args[0]);
+	  }
 		
 	public static void printKundenDB(){
-		//TODO
+		File dir = new File(Main.KUNDENDB);
+
+		File[] children = dir.listFiles();
+		if (children == null) {
+			// Either dir does not exist or is not a directory
+			System.out.println("no files");
+		} else {
+			for (int i=0; i<children.length; i++) {
+				// Get filename of file or directory
+				File file = children[i];
+				if (!file.isDirectory()) {
+					Table t = Table.loadTable(file.getAbsolutePath() + File.separator + file.getName());
+					if (t != null)
+						System.out.print(t);
+				}
+			}
+		}
 	}
 	
 	public static void createKundenDB(){
-		//TODO
+		Main.readFile("kundendb.txt");
 	}
 	
 	public static void execute(String simpleSQL){
-	   //TODO Anfrage übersetzen
-	   //TODO Anfrage ausführen
+		 //TODO Anfrage übersetzen
+		 //TODO Anfrage ausführen
 	}
 	
 	public static ITreeNode sqlToRelationenAlgebra(String simpleSQL){
@@ -33,6 +51,29 @@ public static final String KUNDENDB = "";
 	private static void executePlan(ITreeNode plan){
 		//TODO
 	}
-		
+	
+	private static void readFile(String filename) {
+		File f = new File(filename);
+		if (!f.isFile())
+			return;
+		try{
+			// Open the file that is the first 
+			// command line parameter
+			FileInputStream fstream = new FileInputStream(filename);
+			// Get the object of DataInputStream
+			DataInputStream in = new DataInputStream(fstream);
+				BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			//Read File Line By Line
+			while ((strLine = br.readLine()) != null)	 {
+				// Print the content on the console
+				Main.execute(strLine);
+			}
+			//Close the input stream
+			in.close();
+		}catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+	}
 
 }

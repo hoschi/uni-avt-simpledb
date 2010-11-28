e07069a314bdbeadebec02c82fff29aee5d72199
Jacob Kuypers <jjjk@gmx.li>
2010-11-28 17:37:41 +0100

    [#6016720] Table: select, join und cross implementiert

--- database/Table.java
+++ database/Table.java
@@ -3,164 +3,299 @@ package database;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import relationenalgebra.AndExpression;
+import relationenalgebra.EqualityExpression;
+import relationenalgebra.OrExpression;
+import relationenalgebra.PrimaryExpression;
 
 public class Table implements Serializable {
 
-	static final long serialVersionUID = 1234;
-	protected String name;
-	protected String alias;
-	protected boolean drop;
-	protected List<String> columnNames;
-	protected List<List<String>> rows;
+    private static class ValueLookup {
+        private Map<String,Integer> indexMap;
+        private Table[] tables;
+        private int[] currentRowIndices;
 
-	public Table(String name) {
-		super();
-		this.name = name;
-	}
+        ValueLookup(Table...tables) {
+            indexMap = new HashMap<String,Integer>();
+            this.tables = tables;
+            currentRowIndices = new int[tables.length];
 
-	public Table(String name, List<String> columns) {
-		super();
-		this.name = name;
-		this.columnNames = columns;
-	}
+            for (int i = 0; i < tables.length; i++) {
+                indexMap.put(tables[i].getOfficialName(), Integer.valueOf(i));
+            }
+        }
 
-	/**
-	 * Writes the actual instance to the filesystem.
-	 */
-	public void write() {
-		FileSystemDatabase.getInstance().write(this);
-	}
+        void setCurrentRowIndex(Table table, int rowIndex) {
+            currentRowIndices[indexMap.get(table.getOfficialName())] = rowIndex;
+        }
 
-	/**
-	 * Returns one row from privat row repository.
-	 */
-	public List<String> getRow(int number) {
-		return Collections.unmodifiableList(rows.get(number));
-	}
+        void setCurrentRowIndex(int tableIndex, int rowIndex) {
+            currentRowIndices[tableIndex] = rowIndex;
+        }
 
-	public void addRow(List<String> names) {
-		if (names.size() != columnNames.size()) {
-			throw new IllegalArgumentException("bad row size");
-		}
-		rows.add(cloneList(names));
-	}
+        String lookupValue(String name) {
+            int i = name.indexOf(".");
+            String tableName = name.substring(0, i);
+            String columnName = name.substring(i+1, name.length());
+            Integer tableIndex = indexMap.get(tableName);
 
-	private static <T> List<T> cloneList(List<T> list) {
-		List<T> copy = new ArrayList<T>(list.size());
-		Collections.copy(copy, list);
-		return copy;
-	}
+            if (tableIndex == null) {
+                throw new IllegalArgumentException(
+                        "table '"+tableName+"' not found");
+            }
 
-	public void deleteRow(int number) {
-		rows.remove(number);
-	}
+            Table table = tables[tableIndex];
 
-	private int getColumnIndex(String name) {
-		for (int i = 0; i < columnNames.size(); i++) {
-			if (columnNames.get(i).equals(name)) {
-				return i;
-			}
-		}
-		return -1;
-	}
+            return table.rows.get(currentRowIndices[tableIndex]).get(
+                    table.getColumnIndex(columnName));
+        }
+    }
 
-	private static List<List<String>> newRowsList() {
-		return new LinkedList<List<String>>();
-	}
+    static final long serialVersionUID = 1234;
+    protected String name;
+    protected String alias;
+    protected boolean drop;
+    protected List<String> columnNames;
+    protected List<List<String>> rows;
 
-	public Table projectTo(List<String> param) {
-		// compute indices for projection
-		int[] projection = new int[param.size()];
-		for (int i = 0; i < projection.length; i++) {
-			int index = getColumnIndex(param.get(i));
-			if (index < 0) {
-				throw new IllegalArgumentException("no column with name '"
-						+ param.get(i) + "'");
-			}
-			projection[i] = index;
-		}
+    public Table(String name) {
+        super();
+        this.name = name;
+    }
 
-		// generate column names list
-		List<String> newColumnNames = new ArrayList<String>(projection.length);
-		for (int i = 0; i < projection.length; i++) {
-			newColumnNames.add(columnNames.get(projection[i]));
-		}
+    public Table(String name, List<String> columns) {
+        super();
+        this.name = name;
+        this.columnNames = columns;
+    }
 
-		// generate new rows list
-		List<List<String>> newRows = newRowsList();
-		for (List<String> row : rows) {
-			List<String> projectedRow = new ArrayList<String>(projection.length);
-			for (int i = 0; i < projection.length; i++) {
-				projectedRow.add(row.get(projection[i]));
-			}
-		}
+    /**
+     * Writes the actual instance to the filesystem.
+     */
+    public void write() {
+        FileSystemDatabase.getInstance().write(this);
+    }
 
-		return createAlteredClone(newColumnNames, newRows);
-	}
+    /**
+     * Returns one row from privat row repository.
+     */
+    public List<String> getRow(int number) {
+        return Collections.unmodifiableList(rows.get(number));
+    }
 
-	private Table createAlteredClone(List<String> newColumnNames,
-			List<List<String>> newRows) {
-		Table result = new Table(this.name);
-		result.alias = this.alias;
-		result.columnNames = newColumnNames;
-		result.drop = false;
-		result.rows = newRows;
-		return result;
-	}
+    public void addRow(List<String> names) {
+        if (names.size() != columnNames.size()) {
+            throw new IllegalArgumentException("bad row size");
+        }
+        rows.add(cloneList(names));
+    }
 
-	public Table select(AndExpression exp) {
-		// TODO implement this
-		return null;
-	}
+    private static <T> List<T> cloneList(List<T> list) {
+        List<T> copy = new ArrayList<T>(list.size());
+        Collections.copy(copy, list);
+        return copy;
+    }
 
-	public Table join(Table table, AndExpression exp) {
-		// TODO implement this
-		return null;
-	}
+    public void deleteRow(int number) {
+        rows.remove(number);
+    }
 
-	public Table cross(Table table) {
-		// TODO implement this
-		return null;
-	}
+    private int getColumnIndex(String name) {
+        for (int i = 0; i < columnNames.size(); i++) {
+            if (columnNames.get(i).equals(name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public String toString() {
-		// TODO implement this
-		String s = "table '" + this.name + "'\n\tcolumns: '";
-		for (String c : this.columnNames) {
-			s += c + ", ";
-		}
-		s += "'\n\trows:\n";
+    private static List<List<String>> newRowsList() {
+        return new LinkedList<List<String>>();
+    }
 
-		if (this.rows != null) {
-			for (List<String> row : this.rows) {
-				s += "\t\t";
-				for (String value : row) {
-					s += value + "\t";
-				}
-				s += "\n";
-			}
-		}
-		return s;
-	}
+    public Table projectTo(List<String> param) {
+        // compute indices for projection
+        int[] projection = new int[param.size()];
+        for (int i = 0; i < projection.length; i++) {
+            int index = getColumnIndex(param.get(i));
+            if (index < 0) {
+                throw new IllegalArgumentException("no column with name '"
+                        + param.get(i) + "'");
+            }
+            projection[i] = index;
+        }
 
-	public String getName() {
-		return name;
-	}
+        // generate column names list
+        List<String> newColumnNames = new ArrayList<String>(projection.length);
+        for (int i = 0; i < projection.length; i++) {
+            newColumnNames.add(columnNames.get(projection[i]));
+        }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+        // generate new rows list
+        List<List<String>> newRows = newRowsList();
+        for (List<String> row : rows) {
+            List<String> projectedRow = new ArrayList<String>(projection.length);
+            for (int i = 0; i < projection.length; i++) {
+                projectedRow.add(row.get(projection[i]));
+            }
+        }
 
-	public String getAlias() {
-		return alias;
-	}
+        return createAlteredClone(newColumnNames, newRows);
+    }
 
-	public void setAlias(String alias) {
-		this.alias = alias;
-	}
+    private Table createAlteredClone(List<String> newColumnNames,
+            List<List<String>> newRows) {
+        Table result = new Table(this.name);
+        result.alias = this.alias;
+        result.columnNames = newColumnNames;
+        result.drop = false;
+        result.rows = newRows;
+        return result;
+    }
+
+    public Table select(AndExpression exp) {
+        List<List<String>> newRows = newRowsList();
+        ValueLookup vl = new ValueLookup(this);
+
+        for (int i = 0; i < rows.size(); i++) {
+            vl.setCurrentRowIndex(this, i);
+            if (evaluate(exp, vl)) {
+                newRows.add(cloneList(rows.get(i)));
+            }
+        }
+
+        return createAlteredClone(null, newRows);
+    }
+
+    private boolean evaluate(AndExpression expr, ValueLookup vl) {
+        if (expr.getExprs() == null) {
+            return evaluate(expr.getExpr(), vl);
+        }
+        for (OrExpression or : expr.getExprs()) {
+            if (!evaluate(or, vl)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean evaluate(OrExpression expr, ValueLookup vl) {
+        if (expr.getExprs() == null) {
+            return evaluate(expr.getExpr(), vl);
+        }
+        for (EqualityExpression eq : expr.getExprs()) {
+            if (evaluate(eq, vl)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean evaluate(EqualityExpression expr, ValueLookup vl) {
+        String valueA = retrieveValue(expr.getExpr1(), vl);
+        String valueB = retrieveValue(expr.getExpr1(), vl);
+        int cmp = valueA.compareTo(valueB);
+        switch (expr.getOperator()) {
+        case Equal:
+            return cmp == 0;
+        case Greater:
+            return cmp > 0;
+        case GreaterEqual:
+            return cmp >= 0;
+        case Lower:
+            return cmp < 0;
+        case LowerEqual:
+            return cmp <= 0;
+        case NotEqual:
+            return cmp != 0;
+        default:
+            throw new RuntimeException("unknown operator: "+expr.getOperator());
+        }
+    }
+
+    private String getOfficialName() {
+        if (alias != null) {
+            return alias;
+        }
+        return name;
+    }
+
+    private String retrieveValue(PrimaryExpression expr, ValueLookup vl) {
+        if (expr.isConstant()) {
+            return expr.getValue();
+        }
+        return vl.lookupValue(expr.getValue());
+    }
+
+    public Table join(Table table, AndExpression exp) {
+        List<List<String>> newRows = newRowsList();
+        ValueLookup vl = new ValueLookup(this, table);
+        int newRowSize = columnNames.size() + table.columnNames.size();
+
+        List<String> newColumnNames = new ArrayList<String>(newRowSize);
+        newColumnNames.addAll(columnNames);
+        newColumnNames.addAll(table.columnNames);
+
+        for (int i = 0; i < rows.size(); i++) {
+            vl.setCurrentRowIndex(0, i);
+            for (int j = 0; j < table.rows.size(); j++) {
+                vl.setCurrentRowIndex(1, j);
+                List<String> newRow = new ArrayList<String>(newRowSize);
+                newRow.addAll(rows.get(i));
+                newRow.addAll(table.rows.get(j));
+
+                if (exp == null || evaluate(exp, vl)) {
+                    newRows.add(newRow);
+                }
+            }
+        }
+
+        return createAlteredClone(newColumnNames, newRows);
+    }
+
+    public Table cross(Table table) {
+        return join(table, null);
+    }
+
+    public String toString() {
+        // TODO implement this
+        String s = "table '" + this.name + "'\n\tcolumns: '";
+        for (String c : this.columnNames) {
+            s += c + ", ";
+        }
+        s += "'\n\trows:\n";
+
+        if (this.rows != null) {
+            for (List<String> row : this.rows) {
+                s += "\t\t";
+                for (String value : row) {
+                    s += value + "\t";
+                }
+                s += "\n";
+            }
+        }
+        return s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
 
 }
--- relationenalgebra/EqualityExpression.java
+++ relationenalgebra/EqualityExpression.java
@@ -1,67 +1,79 @@
 package relationenalgebra;
 
 public class EqualityExpression implements IBooleanExpression {
-	
-	public enum Operator {
-		Equal("="),
-		NotEqual("!="),
-		Greater(">"),
-		Lower("<"),
-		LowerEqual("<="),
-		GreaterEqual(">="),
-		;
-		
-		private String opString;
-		
-		Operator(String opString) {
-			this.opString = opString;
-		}
-		
-		public static Operator parseOperator(String str) {
-			for (Operator op : Operator.values()) {
-				if (op.opString.equals(str)) {
-					return op;
-				}
-			}
-			throw new IllegalArgumentException("no Operator matching the string '"+str+"' found");
-		}
-	}
-	
-	private PrimaryExpression expr1, expr2;
-	
-	private Operator operator;
 
-	public EqualityExpression(
-			Operator operator, 
-			PrimaryExpression expr1,
-			PrimaryExpression expr2) {
-		this.operator = operator;
-		this.expr1 = expr1;
-		this.expr2 = expr2;
-	}
+    public enum Operator {
+        Equal("="),
+        NotEqual("!="),
+        Greater(">"),
+        Lower("<"),
+        LowerEqual("<="),
+        GreaterEqual(">="),
+        ;
 
-	@Override
-	public Object evaluate(Relation a, Relation b) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        private String opString;
 
-	@Override
-	public Object evaluate(Relation r) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        Operator(String opString) {
+            this.opString = opString;
+        }
 
-	@Override
-	public String toString() {
-		String s = "";
-		if (expr1 != null && expr1.getValue() != null)
-			s += expr1.getValue();
-		if (this.operator != null)
-			s += " " + this.operator + " ";
-		if (expr2 != null && expr2.getValue() != null)
-			s += expr2.getValue();
-		return s;
-	}
-	
+        public static Operator parseOperator(String str) {
+            for (Operator op : Operator.values()) {
+                if (op.opString.equals(str)) {
+                    return op;
+                }
+            }
+            throw new IllegalArgumentException("no Operator matching the string '"+str+"' found");
+        }
+    }
+
+    private PrimaryExpression expr1, expr2;
+
+    private Operator operator;
+
+    public EqualityExpression(
+            Operator operator,
+            PrimaryExpression expr1,
+            PrimaryExpression expr2) {
+        this.operator = operator;
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+    }
+
+    public PrimaryExpression getExpr1() {
+        return expr1;
+    }
+
+    public PrimaryExpression getExpr2() {
+        return expr2;
+    }
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    @Override
+    public Object evaluate(Relation a, Relation b) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object evaluate(Relation r) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+        if (expr1 != null && expr1.getValue() != null)
+            s += expr1.getValue();
+        if (this.operator != null)
+            s += " " + this.operator + " ";
+        if (expr2 != null && expr2.getValue() != null)
+            s += expr2.getValue();
+        return s;
+    }
+
 }

889d26294d7df7f569f48f6f3667437203100770
Jacob Kuypers <jjjk@gmx.li>
2010-12-31 20:58:27 +0100

    fixed cascaded select creation

--- optimization/CascadeSelects.java
+++ optimization/CascadeSelects.java
@@ -3,7 +3,9 @@ package optimization;
 import java.util.ListIterator;
 
 import relationenalgebra.AndExpression;
+import relationenalgebra.IOneChildNode;
 import relationenalgebra.ITreeNode;
+import relationenalgebra.ITwoChildNode;
 import relationenalgebra.OrExpression;
 import relationenalgebra.Selection;
 
@@ -12,13 +14,21 @@ public class CascadeSelects implements IOptimization {
     @Override
     public ITreeNode optimize(ITreeNode plan) {
         if (!(plan instanceof Selection)) {
+            if (plan instanceof ITwoChildNode) {
+                ((ITwoChildNode) plan).setSecondChild(
+                        optimize(((ITwoChildNode) plan).getSecondChild()));
+            }
+            if (plan instanceof IOneChildNode) {
+                ((IOneChildNode) plan).setChild(
+                        optimize(((IOneChildNode) plan).getChild()));
+            }
             return plan;
         }
         Selection s = (Selection)plan;
 
         // create a cascade
         ListIterator<OrExpression> iter = s.getExpr().getExprs().listIterator();
-        ITreeNode result = s.getChild();
+        ITreeNode result = optimize(s.getChild());
         while (iter.hasNext()) {
             Selection nextSelection = new Selection(
                     new AndExpression(iter.next()));

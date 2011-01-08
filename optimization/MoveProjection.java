70e970f15559834f0f19bd109d2b6983f9d077d5
Jacob K <jacob.kuypers@gmx.de>
2011-01-08 21:10:36 +0100

    completed MoveProjection

--- optimization/MoveProjection.java
+++ optimization/MoveProjection.java
@@ -1,9 +1,16 @@
 package optimization;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import relationenalgebra.CrossProduct;
 import relationenalgebra.IOneChildNode;
 import relationenalgebra.ITreeNode;
 import relationenalgebra.ITwoChildNode;
+import relationenalgebra.Join;
 import relationenalgebra.Projection;
+import relationenalgebra.Selection;
 
 public class MoveProjection implements IOptimization {
 
@@ -27,7 +34,85 @@ public class MoveProjection implements IOptimization {
     }
 
     private static ITreeNode moveProjection(Projection p) {
-        return null;
+        ITreeNode child = p.getChild();
+        if (child instanceof Selection) {
+        	// always switch with selection
+        	Selection s = (Selection)child;
+        	p.setChild(s.getChild());
+        	s.setChild(p);
+        	return s;
+        } else if (child instanceof CrossProduct) {
+        	// handle cross product or join
+        	
+        	/* model:
+        	 * 					  		 /-> firstChild
+        	 * rootNode -> twoChildNode -|
+        	 * 					  		 \-> secondChild
+        	 */
+        	ITwoChildNode twoChildNode = (ITwoChildNode)child;
+        	ITreeNode firstChild = twoChildNode.getChild();
+        	ITreeNode secondChild = twoChildNode.getSecondChild();
+        	IOneChildNode rootNode = twoChildNode;
+        	
+        	// attributes from the (input) projection
+        	Set<String> L = new HashSet<String>(p.getColumnnames());
+
+        	// attributes from the children
+        	Set<String> firstChildAttributes = firstChild.getAttributes();
+        	Set<String> projectedFirstChildAttributes = intersect(firstChildAttributes, L);
+        	Set<String> secondChildAttributes = secondChild.getAttributes();
+        	Set<String> projectedSecondChildAttributes = intersect(secondChildAttributes, L);
+        	
+        	// does the join expression contain extra attributes?
+        	if (twoChildNode instanceof Join) {
+        		Set<String> c = ((Join) twoChildNode).getExpr().getAttributes();
+        		if (!L.containsAll(c)) {        			
+        			// retain the current projection
+        			rootNode = p;
+        			
+        			// extend children attributes
+        			projectedFirstChildAttributes.addAll(intersect(firstChildAttributes, c));
+        			projectedSecondChildAttributes.addAll(intersect(secondChildAttributes, c));
+        		}
+        	}
+        	
+        	// create children projections
+        	Projection firstChildProjection = new Projection(new ArrayList<String>(projectedFirstChildAttributes));
+        	Projection secondChildProjection = new Projection(new ArrayList<String>(projectedSecondChildAttributes));
+        	
+        	// move projections
+        	firstChild = moveProjection(firstChildProjection);
+        	secondChild = moveProjection(secondChildProjection);
+        	
+        	// update relations and return result
+        	twoChildNode.setChild(firstChild);
+        	twoChildNode.setSecondChild(secondChild);
+        	return rootNode;
+        } else {
+        	return p; // cannot move
+        }
     }
+
+    /**
+     * Intersects two sets
+     * 
+     * @param setA
+     * @param setB
+     * @return intersection of setA and setB
+     */
+	private static <T> Set<T> intersect(Set<T> setA, Set<T> setB) {
+		Set<T> result = new HashSet<T>();
+		if (setA.size() > setB.size()) {
+			Set<T> temp = setB;
+			setB = setA;
+			setA = temp;
+		}
+		for (T s : setA) {
+			if (setB.contains(s)) {
+				result.add(s);
+			}
+		}
+		return result;
+	}
 
 }

127322209d5d2129516055c6fdbb9f5cf76431e6
Jacob Kuypers <jacob.kuypers@gmx.de>
2011-01-10 11:14:10 +0100

    implemented block 2 test run (has errors)

--- main/Main.java
+++ main/Main.java
@@ -9,7 +9,10 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 
 import optimization.CascadeSelects;
-
+import optimization.DetectJoins;
+import optimization.IOptimization;
+import optimization.MoveProjection;
+import optimization.MoveSelection;
 import parser.gene.ParseException;
 import parser.gene.SimpleSQLParser;
 import parser.syntaxtree.CompilationUnit;
@@ -23,7 +26,6 @@ import relationenalgebra.Projection;
 import relationenalgebra.Relation;
 import relationenalgebra.Selection;
 import relationenalgebra.TableOperation;
-import test.TreeNodeTester;
 import database.FileSystemDatabase;
 import database.Table;
 
@@ -34,23 +36,116 @@ public class Main {
 
 	public static void main(String[] args) throws IOException,
 			ClassNotFoundException {
-		Logger.debug = true;
-		Logger.debug("DEBUGGING IS ENABLED");
+		//Logger.debug = true;
+		//Logger.debug("DEBUGGING IS ENABLED");
 		Logger.debug("load database");
 		FileSystemDatabase.getInstance().setDbDirectory(KUNDENDB);
 		Main.createKundenDB();
 		Logger.debug("execute sql");
 		//Main.execute("select B.Titel from Buch_Autor as BA, Buch as B where BA.Autorenname=\"Christian Ullenboom\" and BA.B_ID=B.ID");
 		//Main.execute("select B.Titel from Buch_Autor as BA, Buch as B where BA.Autorenname=\"Henning Mankell\" and BA.B_ID=B.ID");
-		Main.execute("select B.Titel from Buch as B, Kunde as K, Buch_Bestellung as BB, Kunde_Bestellung as KB where K.Name=\"KName1\" and K.ID=KB.K_ID and KB.B_ID=BB.Be_ID and BB.Bu_ID=B.ID");
+		//Main.execute("select B.Titel from Buch as B, Kunde as K, Buch_Bestellung as BB, Kunde_Bestellung as KB where K.Name=\"KName1\" and K.ID=KB.K_ID and KB.B_ID=BB.Be_ID and BB.Bu_ID=B.ID");
 		
 		//Main.readFile("sql.txt");
-		
+		Main.blockTwoOptimizationDemo();
 		
 		//Main.printKundenDB();
 		//FileSystemDatabase.getInstance().persistDb();
 	}
 	
+	public static void blockTwoOptimizationDemo() {
+		String[] queries = new String[]{
+			"select B.Titel " +
+			"from " +
+			"	Buch as B, " +
+			"	Kunde as K, " +
+			"	Buch_Bestellung as BB," +
+			"	Kunde_Bestellung as KB " +
+			"where " +
+			"	K.Name=\"KName1\" and " +
+			"	K.ID=KB.K_ID and " +
+			"	KB.B_ID=BB.Be_ID and " +
+			"	BB.Bu_ID=B.ID",
+			
+			"select B.ID, K.Name " +
+			"from" +
+			"	Bestellung as B, " +
+			"	Kunde as K, " +
+			"	Kunde_Bestellung as KB, " +
+			"where " +
+			"	KB.K_ID=K.ID and " +
+			"	KB.B_ID=B.ID and " +
+			"	B.ID=\"Bestellung5\"",
+			
+			"select Name " +
+			"from " +
+			"	Kunde, " +
+			"	Kunde_Bestellung " +
+			"where " +
+			"	ID=K_ID and " +
+			"	Name=\"KName1\"",
+		};
+		
+		IOptimization cascadeSelects = new CascadeSelects();
+		IOptimization detectJoins = new DetectJoins();
+		IOptimization moveSelection = new MoveSelection();
+		IOptimization moveProjection = new MoveProjection();
+		
+		String[] titles = new String[]{
+				"Result with no optimizations: ",
+				"Result with cascaded and moved selections: ",
+				"Result with cascaded and moved selections and detected joins: ",
+				"Result with cascaded and moved selections, detected joins and moved projections: ",
+		};
+		
+		IOptimization[][] optimizationLists = new IOptimization[][]{
+				new IOptimization[0],
+				new IOptimization[]{ cascadeSelects, moveSelection },
+				new IOptimization[]{ cascadeSelects, moveSelection, detectJoins },
+				new IOptimization[]{ cascadeSelects, moveSelection, detectJoins, moveProjection },
+		};
+		
+		for (String query : queries) {
+			System.out.println("Next query: ");
+			System.out.println(query);
+			System.out.println();
+			
+			for (int i = 0; i < titles.length; i++) {
+				System.out.println(titles[i]);
+				Table result = executeOptimized(query, optimizationLists[i]);
+				if (result == null) {
+					System.out.println("(no result, optimization failed)");
+				} else {
+					System.out.println(result.toString());
+				}
+				System.out.println();
+			}
+		}
+	}
+	
+	private static Table executeOptimized(String query, IOptimization[] optimizations) {
+		ITreeNode plan = sqlToRelationenAlgebra(query);
+		for (IOptimization optimization : optimizations) {
+			try {
+				plan = optimization.optimize(plan);
+			} catch (Exception e) {
+				System.err.println("failed to optimize query using "+optimization);
+				System.err.println(e.getMessage());
+				e.printStackTrace();
+				return null;
+			}
+		}
+		try {
+			return executeQuery(plan);
+		} catch (Exception e) {
+			System.err.println("failed to execute query");
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+
 	public static void printKundenDB() throws IOException,
 			ClassNotFoundException {
 		FileSystemDatabase.getInstance().printDb();
